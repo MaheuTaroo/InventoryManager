a22fbdb265b79f036a429231d4e50bd7ddd96084
MaheuTaroo <58953303+MaheuTaroo@users.noreply.github.com>
2023-10-28 12:23:23 +0100

    Recursos e ecrã de login

---
+++ InventoryManager.UI/Login.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace InventoryManager.UI
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            btnLogin = new Button();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel4 = new TableLayoutPanel();
+            label2 = new Label();
+            textBox4 = new TextBox();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            txtUser = new TextBox();
+            label1 = new Label();
+            pbLogin = new PictureBox();
+            tableLayoutPanel3 = new TableLayoutPanel();
+            textBox3 = new TextBox();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel4.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pbLogin).BeginInit();
+            SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            btnLogin.Anchor = AnchorStyles.None;
+            btnLogin.Location = new Point(141, 282);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(77, 39);
+            btnLogin.TabIndex = 0;
+            btnLogin.Text = "Log in";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel4, 0, 2);
+            tableLayoutPanel1.Controls.Add(btnLogin, 0, 3);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 1);
+            tableLayoutPanel1.Controls.Add(pbLogin, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 4;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 49.9975F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 15.0007505F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 15.0007505F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20.0010014F));
+            tableLayoutPanel1.Size = new Size(359, 336);
+            tableLayoutPanel1.TabIndex = 3;
+            // 
+            // tableLayoutPanel4
+            // 
+            tableLayoutPanel4.ColumnCount = 2;
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 27.5F));
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 72.5F));
+            tableLayoutPanel4.Controls.Add(label2, 0, 0);
+            tableLayoutPanel4.Controls.Add(textBox4, 1, 0);
+            tableLayoutPanel4.Dock = DockStyle.Fill;
+            tableLayoutPanel4.Location = new Point(3, 220);
+            tableLayoutPanel4.Name = "tableLayoutPanel4";
+            tableLayoutPanel4.RowCount = 1;
+            tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel4.Size = new Size(353, 44);
+            tableLayoutPanel4.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.None;
+            label2.AutoSize = true;
+            label2.Location = new Point(20, 14);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Password";
+            // 
+            // textBox4
+            // 
+            textBox4.Anchor = AnchorStyles.None;
+            textBox4.Location = new Point(121, 10);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(207, 23);
+            textBox4.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 2;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 27.5F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 72.5F));
+            tableLayoutPanel2.Controls.Add(txtUser, 1, 0);
+            tableLayoutPanel2.Controls.Add(label1, 0, 0);
+            tableLayoutPanel2.Dock = DockStyle.Fill;
+            tableLayoutPanel2.Location = new Point(3, 170);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 1;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel2.Size = new Size(353, 44);
+            tableLayoutPanel2.TabIndex = 0;
+            // 
+            // txtUser
+            // 
+            txtUser.Anchor = AnchorStyles.None;
+            txtUser.Location = new Point(121, 10);
+            txtUser.Name = "txtUser";
+            txtUser.Size = new Size(208, 23);
+            txtUser.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.Location = new Point(33, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(30, 15);
+            label1.TabIndex = 3;
+            label1.Text = "User";
+            // 
+            // pbLogin
+            // 
+            pbLogin.Anchor = AnchorStyles.None;
+            pbLogin.Image = Properties.Resources.Login;
+            pbLogin.InitialImage = null;
+            pbLogin.Location = new Point(115, 19);
+            pbLogin.Name = "pbLogin";
+            pbLogin.Size = new Size(128, 128);
+            pbLogin.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbLogin.TabIndex = 5;
+            pbLogin.TabStop = false;
+            // 
+            // tableLayoutPanel3
+            // 
+            tableLayoutPanel3.ColumnCount = 2;
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.Dock = DockStyle.Fill;
+            tableLayoutPanel3.Location = new Point(0, 0);
+            tableLayoutPanel3.Name = "tableLayoutPanel3";
+            tableLayoutPanel3.RowCount = 1;
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel3.Size = new Size(200, 100);
+            tableLayoutPanel3.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            textBox3.Anchor = AnchorStyles.None;
+            textBox3.Location = new Point(103, 38);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(94, 23);
+            textBox3.TabIndex = 1;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(359, 336);
+            Controls.Add(tableLayoutPanel1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Login";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel4.ResumeLayout(false);
+            tableLayoutPanel4.PerformLayout();
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pbLogin).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnLogin;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private TableLayoutPanel tableLayoutPanel4;
+        private TextBox textBox4;
+        private TableLayoutPanel tableLayoutPanel3;
+        private TextBox textBox3;
+        private TextBox txtUser;
+        private Label label1;
+        private Label label2;
+        private PictureBox pbLogin;
+    }
+}
